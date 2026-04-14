96c7087ef46f0cd2d261d127de1679e1e30f2ee5
nirayndra <faadiajasmine@gmail.com>
2026-04-14 11:11:42 +0700

    deklarasi while untuk jumlah elemen pada array

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -7,5 +7,18 @@ int x;
 
 void input()
 {
-    
+    while(true)
+    {
+        cout << "Masukkan banyak elemen pada array: ";
+        cin >> nPanjang;
+
+        if (nPanjang <= 10)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\n[!] Jumlah element tidak boleh lebih dari 10. Silahkan coba lagi.\n";
+        }
+    }
 }
