46923492839eb3b66110555b8d90a6d68a9de0ba
nirayndra <faadiajasmine@gmail.com>
2026-04-14 11:16:11 +0700

    menampilkan hasil setelah diurutkan

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -55,4 +55,14 @@ void display()
     cout << "\n=============================================\n";
     cout << "\n====Element Array Setelah Diurutkan (ASC)====\n";
     cout << "\n=============================================\n";
+
+    for (int j = 0; j < nPanjang; j++)
+    {
+        cout << element[j];
+        if (j < nPanjang - 1)
+        {
+            cout << " -> ";
+        }
+    }
+    cout << endl;
 }
